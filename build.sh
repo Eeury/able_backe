089a3dd9e8239ea6c0b4d1c3e6dca5a9bcb66e1d
Eeury <callmeeury@gmail.com>
2025-11-25 11:09:12 +0300

    allowed hosts

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run database migrations
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-
-# Collect static files
-python manage.py collectstatic --no-input --clear
-
